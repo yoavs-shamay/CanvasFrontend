8c41525a54667ea2ab53ac53fb0ce7cf1f98bba7
yoav-shamay <yoav.s@bengurion.org>
2022-04-10 12:17:55 +0300

    changed parseerror to parsererror

--- Scripts/login.js
+++ Scripts/login.js
@@ -2,7 +2,7 @@ function onSignIn(googleUser)
 {
     var idToken = googleUser.getAuthResponse().id_token;
     backendHttpRequest("Login", { "googleToken": idToken }, function(data, status) {
-        if (status == "parseerror")
+        if (status == "parsererror")
         {
             localStorage.setItem("sessionId", data);
             window.location.href = "index.html";
