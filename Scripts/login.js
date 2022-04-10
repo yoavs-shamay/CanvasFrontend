19bb3341e774670a71d727198ee3657ba548ed48
yoav-shamay <yoav.s@bengurion.org>
2022-04-10 12:12:10 +0300

    added login failed message

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,8 +1,15 @@
 function onSignIn(googleUser)
 {
     var idToken = googleUser.getAuthResponse().id_token;
-    backendHttpRequest("Login", { "googleToken": idToken }, function(data) {
-        localStorage.setItem("sessionId", data);
-        window.location.href = "index.html";
+    backendHttpRequest("Login", { "googleToken": idToken }, function(data, status) {
+        if (status == "success")
+        {
+            localStorage.setItem("sessionId", data);
+            window.location.href = "index.html";
+        }
+        else
+        {
+            $("#failed-message").show();
+        }
     });
 }
